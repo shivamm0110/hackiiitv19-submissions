3411b30f47a4a6b7f57982e7311bb6666c9735a8
shivamm0110 <201951143@iiitvadodara.ac.in>
2019-10-05 17:09:07 +0530

    team#2 code 1

--- team#2 - Food Technology/ft_team#2.cpp
+++ team#2 - Food Technology/ft_team#2.cpp
@@ -1,6 +1,8 @@
-#include<iostream.h>
+#include<iostream>
+#include<iomanip>
 #include<conio.h>
 
+using namespace std;
 static int no=1;
 
 class booth{
@@ -8,25 +10,41 @@ class booth{
         int f_id;
         float wt;
         int grade;
+        float price=0;
     public:
         void in(){
             cout<<"Enter farmer id: ";
             cin>>f_id;
-            cout<<"Enter weight of crop";
+            cout<<"Enter weight of crop: ";
             cin>>wt;
-            cout<<"Enter its quality";
+            cout<<"Enter its quality(between 1-3): ";
             cin>>grade;
+        }
+        void process(){
+            switch(grade){
+                    case 1: price=wt*1/3*100;
+                            break;
+                    case 2: price=wt*2/3*100;
+                            break;
+                    case 3: price=wt*100;
+                            break;
+                    default:cout<<"wrong input of quality ";
+                            break;
+            }
+
         }
         void out(){
 
             no++;
         }
 
-}
+};
 
 class consumer{
 
-}
-void main(){
+};
+int main(){
     booth  b;
+    b.in();
+    return 0;
 }
