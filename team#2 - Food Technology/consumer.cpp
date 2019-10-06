860aaea583a45b1c19c379d9917587cc509acf5a
shivamm0110 <201951143@iiitvadodara.ac.in>
2019-10-06 09:07:35 +0530

    team#2 both

--- team#2 - Food Technology/consumer.cpp
+++ team#2 - Food Technology/consumer.cpp
@@ -13,34 +13,91 @@ class consumer{
         float grade;
         float price;
         float dis,c_price;
+        float c_wt;
 
     public:
+        int ffun(){
+            return f_id;
+        }
+        float gfun(){
+            return grade;
+        }
         void out(){
-            cout<<f_id <<endl;
-            printf("%.2f\n",wt);
-            cout<<grade<<endl;
-            printf("%.2f\n",price);
+            cout<<"farmer ID "<<f_id <<endl;
+            printf("Weight of crop in Quintal%.2f\n",wt);
+            cout<<"Quality of crop: "<<grade<<endl;
+           // printf("%.2f\n",price);
         }
         void process(){
                 cout << "Please enter the distance from booth in KM ";
                 cin>>dis;
-                if(dis<=500)
-                    c_price=price+2400+(dis*wt*5);
-                else
-                    c_price=price+2400+(dis*wt*3);
-                cout<<c_price;
+                cout<<endl << "Enter crop amount required in Quintal";
+                cin>>c_wt;
+                if(wt>=c_wt){
+                    if(dis<=500)
+                        c_price=price+2400+(dis*c_wt*5);
+                    else
+                        c_price=price+2400+(dis*c_wt*3);
+                cout<<"Price to be paid: "<<c_price<<endl<<endl;
+                }
+                else{
+                    cout<< "Insufficient stock";
+                }
+                system("pause");
+
 
         }
 
 };
 int main(){
-        ifstream fin("data.dat",ios::in);
-            while(fin){
-                consumer c;
-                fin.read((char*)&c,sizeof(c));
-                c.out();
-                c.process();
-            }
+        int x,f;
+        float g;
+        while(1){
+            ifstream fin("data.dat",ios::in);
+            system("cls");
+            cout<<"1> Display whole list: \n2>Search by farmer ID:\n3>Search by Grade:\n4>Exit \n\nEnter the option number you want to execute: ";
+            cin>>x;
+            switch(x){
+                case(1):
+                        while(fin){
+                            consumer c;
+                            fin.read((char*)&c,sizeof(c));
+                            c.out();
+                            c.process();
+                        }
+                        break;
+                case(2):cout<<"Enter farmer id to be searched: ";
+                        cin>>f;
+                         while(fin){
+                            consumer c;
+                            fin.read((char*)&c,sizeof(c));
+
+                            if(c.ffun()==f){
+                                c.out();
+                                c.process();
+                            }
+                        }
+                        break;
+                case(3):cout<<"Enter quality grade to be searched: ";
+                        cin>>g;
+                         while(fin){
+                            consumer c;
+                            fin.read((char*)&c,sizeof(c));
+                            if(c.gfun()==g){
+                                c.out();
+                                c.process();
+                            }
+                        }
+                        break;
+                case(4):cout<<"Exiting... ";
+                        exit(1);
+                        break;
+                default:cout<<"Wrong input ";
+                        break;
+                }
             fin.close();
-    return 0;
+            }
+
+
+return 0;
 }
